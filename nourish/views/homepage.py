07bdf2b2befd35c55fee33d7a8e248625bd4d550
Marcus Schwartz <marcus@marcus.net>
2011-04-22 08:29:34 -0700

    Fixes issue 13.

--- nourish/views/homepage.py
+++ nourish/views/homepage.py
@@ -5,14 +5,22 @@ from django.template import RequestContext
 
 def homepage(request):
     groups = [ ]
-    for gm in GroupUser.objects.filter(user=request.user):
-        groups.append(gm.group)
+    events = []
+    if request.user.is_authenticated():
+        for gm in GroupUser.objects.filter(user=request.user):
+            groups.append(gm.group)
 
-    events = list(EventGroup.objects.filter(group__in=groups))
+        egs = list(EventGroup.objects.filter(group__in=groups))
+
+        for eu in EventUser.objects.filter(user=request.user):
+            events.append(eu.event)
+    else:
+        egs = None
 
     return render_to_response('nourish/homepage.html', { 
         'request': request,
         'events' : events,
+        'egs' : egs,
         'groups' : groups,
     }, context_instance=RequestContext(request))
 
@@ -31,10 +39,11 @@ def homepage_chooser(request):
     except UserProfile.DoesNotExist:
         return redirect('/home')
     if profile.role == 'E':
-        try:
-            eu = EventUser.objects.get(user=user,admin=True)
-        except EventUser.DoesNotExist:
-            return redirect('/events/create')
+        eus = EventUser.objects.filter(user=user,admin=True)
+        if len(eus) > 1:
+            return redirect('/home')
+        if not len(eus):
+            return redirect('/register/event/')
         return redirect(eu.event.get_absolute_url())
     if profile.role == 'T':
         gus = GroupUser.objects.filter(user=user,admin=True)
@@ -51,7 +60,10 @@ def homepage_chooser(request):
         if len(gus) > 1:
             return redirect('/home')
         if not len(gus):
-            return redirect('/groups/create?guest')
+            if 'nourish_event' in request.session:
+                event = Event.objects.get(id = request.session['nourish_event'])
+                return redirect(event.get_absolute_url() + 'register/guest/')
+            return redirect('/events/')
         egs = EventGroup.objects.filter(group=gus[0].group)
         if len(egs) == 1:
             return redirect(egs[0].get_absolute_url())
