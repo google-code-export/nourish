399d3158cffd6cbdfe7c52d154de54ab31e3e723
Marcus Schwartz <marcus@marcus.net>
2011-04-24 11:31:28 -0700

    validation for event dates

--- nourish/forms/event.py
+++ nourish/forms/event.py
@@ -2,9 +2,21 @@ from django import forms
 from django.forms import ModelForm
 from django.forms.extras.widgets import SelectDateWidget
 from nourish.models import Event
+from datetime import date, timedelta
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
     start_date = forms.DateField(widget=SelectDateWidget)
     end_date = forms.DateField(widget=SelectDateWidget)
+
+    def clean(self):
+        start = self.cleaned_data.get("start_date")
+        end = self.cleaned_data.get("end_date")
+        if start < date.today():
+            raise forms.ValidationError("start date must be in the future!")
+        if start > end:
+            raise forms.ValidationError("start date must be after end date!")
+        if (end - start) > timedelta(days=28):
+            raise forms.ValidationError("event must not be longer than four weeks")
+        return self.cleaned_data
