ffc6f40aa5b4d98303efef045c10af20af9798c6
Marcus Schwartz <marcus@marcus.net>
2011-05-04 23:19:48 -0700

    missed file

---
+++ nourish/views/canvas.py
@@ -0,0 +1,12 @@
+class HybridCanvasView(object):
+    def get(self, request, **kwargs):
+        if 'canvas' in kwargs:
+            setattr(self, 'canvas', True)
+        return super(HybridCanvasView, self).get(request, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(HybridCanvasView, self).get_context_data(**kwargs)
+        if hasattr(self,'canvas'):
+            context['canvas'] = self.canvas
+        return context
+
