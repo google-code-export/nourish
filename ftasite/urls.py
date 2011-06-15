bcef071aa4f803e81316eebf5011a8f96a1c7fd9
Ray Marceau <razor2n4@gmail.com>
2011-06-15 17:38:54 -0400

    removed event register link and replaced with email contact to colonel angus.  Added some copy

---
+++ ftasite/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login, logout
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    url(r'^nourish/', include('nourish.urls')),
+    url(r'^nourish/fb/', include('nourish.urls', namespace='fbcanvas'), { 'fbcanvas' : True }),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^social/', include('socialregistration.urls')),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    (r'^donate/$', direct_to_template, {'template': 'site/donate.html'}),
+    (r'^$', direct_to_template, {'template': 'site/home.html'}),
+    (r'^video/$', direct_to_template, {'template': 'site/video.html'}),
+    (r'^archives/$', direct_to_template, {'template': 'site/archives.html'}),
+)
