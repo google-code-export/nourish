8c6829c30869a0d2389b22d463c1dc669a9e73b2
razor2n4 <razor2n4@gmail.com>
2011-06-20 20:17:28 +0000

    escaped strings for JS

---
+++ nourish/views/event.py
@@ -0,0 +1,104 @@
+from django.views.generic import DetailView, UpdateView, ListView
+from nourish.models import EventGroup, Event, GroupUser, Meal, MealInvite, EventUser
+from nourish.forms import EventForm, EventGroupHostForm, EventInviteDayForm, EventInviteMealForm
+from nourish.forms.event import EventGroupHostFeaturesForm, EventGroupHostForm
+from nourish.forms.register import RegistrationStubForm
+from nourish.forms.group import GroupForm, GroupFBForm
+from django.shortcuts import get_object_or_404, redirect
+from datetime import timedelta
+from fbcanvas.views import HybridCanvasView
+from nourish.views.register.host import EventHostRegisterView
+from django.forms.formsets import formset_factory
+import json
+
+import sys
+from pprint import pformat
+import pprint
+
+class EventDetailView(HybridCanvasView, DetailView):
+    context_object_name = 'event'
+    model = Event
+    template_name='nourish/EventDetailView.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EventDetailView, self).get_context_data(**kwargs)
+        context['host_list'] = []
+        context['guest_list'] = []
+        context['is_admin'] = self.object.is_admin(self.request.user)
+        context['eventuser_list'] = EventUser.objects.filter(event=self.object,admin=True)
+        for eg in EventGroup.objects.filter(event=self.object):
+            if eg.group.role == 'T':
+                context['host_list'].append(eg)
+            if eg.group.role == 'A':
+                context['guest_list'].append(eg)
+        return context
+
+class EventGroupView(HybridCanvasView, DetailView):
+    context_object_name = 'event_group'
+    model = EventGroup
+    template_name='nourish/EventGroupView.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(EventGroupView, self).get_context_data(**kwargs)
+        context['meals'] = Meal.objects.filter(eg=self.object).order_by('date', 'meal')
+        context['invites_sent'] = MealInvite.objects.filter(host_eg=self.object)
+        context['invites_rcvd'] = MealInvite.objects.filter(guest_eg=self.object)
+        context['is_group_admin'] = self.object.group.is_admin(self.request.user)
+        context['is_event_admin'] = self.object.event.is_admin(self.request.user)
+        dates = []
+        date = self.object.event.start_date
+        while date <= self.object.event.end_date:
+            dates.append(date)
+            date += timedelta(days=1)
+        context['dates'] = dates
+        groups = []
+        group_admin = True;
+        if self.request.user.is_authenticated():
+            context['is_admin'] = self.object.group.is_admin(self.request.user)
+            for g in GroupUser.objects.filter(user=self.request.user,admin=True):
+                if g.group.role == 'T':
+                    groups.append(g.group)
+            host_egs = EventGroup.objects.filter(group__in=groups,event=self.object.event)
+        else:
+            context['is_admin'] = False
+            host_egs = []
+        
+#        host_egs = EventGroup.objects.filter(role='T', group__in=groups)
+        context['host_event_groups'] = host_egs
+        return context
+
+class EventUpdateView(HybridCanvasView, UpdateView):
+    context_object_name = 'event'
+    model = Event
+    form_class = EventForm
+    login_required = True
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().is_admin(self.request.user):
+            raise PermissionDenied
+        return super(EventUpdateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().is_admin(self.request.user):
+            raise PermissionDenied
+        return super(EventUpdateView, self).post(request, *args, **kwargs)
+
+class EventListView(HybridCanvasView, ListView):
+    template_name='nourish/EventListView.html'
+    queryset=Event.objects.filter(display=True)
+    
+class EventGroupUpdateView(HybridCanvasView, UpdateView):
+    context_object_name = 'eventgroup'
+    model = EventGroup
+    form_class = EventGroupHostForm
+    login_required = True
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().group.is_admin(self.request.user):
+            raise PermissionDenied
+        return super(EventGroupUpdateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().group.is_admin(self.request.user):
+            raise PermissionDenied
+        return super(EventGroupUpdateView, self).post(request, *args, **kwargs)
---
+++ nourish/views/group.py
@@ -0,0 +1,50 @@
+from django.views.generic import DetailView, UpdateView, ListView
+from nourish.models import Group, EventGroup, GroupUser
+from nourish.forms import GroupForm
+from django.core.exceptions import PermissionDenied
+from pprint import pformat
+import sys
+import os
+
+sys.stderr.write("PATH " + os.getcwd() + "\n")
+
+from fbcanvas.views import HybridCanvasView
+
+class GroupDetailView(HybridCanvasView, DetailView):
+    context_object_name = 'group'
+    model = Group
+    template_name='nourish/GroupDetailView.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupDetailView, self).get_context_data(**kwargs)
+        context['is_admin'] = self.object.is_admin(self.request.user)
+        context['eventgroup_list'] = EventGroup.objects.filter(group=self.object)
+    	context['groupuser_list'] = GroupUser.objects.filter(group=self.object)
+	try:
+	    context['my_membership'] = GroupUser.objects.get(group=self.object,user=self.request.user)
+	except GroupUser.DoesNotExist:
+	    context['my_membership'] = None
+	except TypeError:
+	    context['my_membership'] = None
+	return context
+
+
+class GroupUpdateView(HybridCanvasView, UpdateView):
+    context_object_name = 'group'
+    model = Group
+    form_class = GroupForm
+    login_required = True
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().is_admin(self.request.user):
+            raise PermissionDenied
+        return super(GroupUpdateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated() and not self.get_object().is_admin(self.request.user):
+            raise PermissionDenied
+        return super(GroupUpdateView, self).post(request, *args, **kwargs)
+
+class GroupListView(HybridCanvasView, ListView):
+    model=Group
+    template_name='nourish/GroupListView.html'
