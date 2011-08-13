c4233654dbda7e93e231918efd48385768dd466a
Ray Marceau <razor2n4@gmail.com>
2011-08-13 13:06:22 -0400

    Admin interface enhancements

--- nourish/admin.py
+++ nourish/admin.py
@@ -7,15 +7,61 @@ from nourish.models import Meal
 from nourish.models import MealInvite
 from nourish.models import UserProfile
 from nourish.models import FacebookProfileCache
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from django.contrib import admin
 
-admin.site.register(Group)
-admin.site.register(GroupUser)
+class EventGroupInline(admin.TabularInline):
+    model = EventGroup
+
+class EventGroupAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'event', 'group', 'role')
+    list_filter = ('event__name', 'role')
+    list_per_page = 500
+    ordering = ['-event', 'role', 'group']
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'role')
+    list_per_page = 500
+    inlines = [
+        EventGroupInline,
+    ]
+
+class MealInline(admin.TabularInline):
+    model = Meal
+
+class MealInviteAdmin(admin.ModelAdmin):
+    list_display = ('date', 'event', 'host_eg', 'guest_eg', 'dinner_time', 'state')
+    list_filter = ('date', 'event__name', 'state')
+    list_per_page = 500
+    inlines = [
+        MealInline,
+    ]
+
+class UserInline(admin.TabularInline):
+    model = User
+
+class GroupUserAdmin(admin.ModelAdmin):
+    list_per_page = 500
+    inlines = [
+        UserInline,
+    ]
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'invite', 'state', 'members')
+    list_filter = ('state', 'event__name')
+    list_per_page = 500
+
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login', 'date_joined')
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(GroupUser, GroupUserAdmin)
 admin.site.register(Event)
 admin.site.register(EventUser)
-admin.site.register(EventGroup)
-admin.site.register(Meal)
-admin.site.register(MealInvite)
+admin.site.register(EventGroup, EventGroupAdmin)
+admin.site.register(Meal, MealAdmin)
+admin.site.register(MealInvite, MealInviteAdmin)
 admin.site.register(UserProfile)
 admin.site.register(FacebookProfileCache)
